a319c9e8e3b95ee501cde322e7b7a5fa6c838604
Tourette137 <47273656+Tourette137@users.noreply.github.com>
2019-05-20 23:05:24 +0100

    Update movimentoValidos.c

--- Projeto_LI/movimentoValidos.c
+++ Projeto_LI/movimentoValidos.c
@@ -10,38 +10,12 @@
 #include <stdlib.h>
 
 
-/*
-// Função auxiliar da função seguinte, que é introduzida da mesma maneira!
-int verifica_Movimento (VALOR who, int vl, int vc, int l, int c, ESTADO *e) {
 
-    if ((*e).grelha[l][c] == who) return 1;
-
-    if ((vc+c<0) || (vc+c>7)) return 0;
-
-    if ((vl+l<0) || (vl+l>7)) return 0;
-
-    return verifica_Movimento(who,vl,vc,l+vl,c+vl,e);
-}
-
-
-// Função que vê numa direção se movimento é valido
-int movimento_valido (VALOR who, int vl, int vc, int l, int c, ESTADO *e) {
-    VALOR adversario;
-    if (who == VALOR_O) (adversario = VALOR_X);
-    else if (who == VALOR_X) (adversario = VALOR_O);
-
-    if ( (vl+l<0) || (vl+l>7) || (vc+c<0) || (vc+c>7) ) return 0;
-
-    if ((*e).grelha[vl+l][vc+c] != adversario) return 0;
-
-    if ( (vl+vl+l<0) || (vl+vl+l>7) || (vc+vc+c<0) || (vc+vc+c>7) ) return 0;
-
-    return verifica_Movimento (who, l, c, l+vl+vl, c+vc+vc, e);
-}
+/**
+ *
+ * @param e - Estado de jogo atual
+ * @return Estado de jogo com todos os movimentos váldos possíveis de realizar por parte do jogador em jogo
  */
-
-
-// Função que vai returnar um estado com as posições que possam ser sugeridas para movimentos validos
 ESTADO calculaMovimentosValidos (ESTADO e) {
 
     for (int l = 0; l < 8; l++) {
@@ -75,7 +49,12 @@ ESTADO calculaMovimentosValidos (ESTADO e) {
 }
 
 
-// Recebe um estado com as possibilidades de jogadas e escolhe, aleatoriamente, uma delas
+
+/**
+ *
+ * @param e - Estado de jogo atual
+ * @return Estado com as que vai escolher uma jogada possível aleatoriamente e a vai sugerir ao jogador
+ */
 ESTADO hint (ESTADO e)
 {
     int x = 0;
